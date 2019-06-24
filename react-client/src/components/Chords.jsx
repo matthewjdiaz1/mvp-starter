bc8e8bb876f59e344cf6b637fecca8c0645cfaaf
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-06-24 15:06:46 -0700

    added accompanying chords and piano visualization

---
+++ react-client/src/components/Chords.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+
+class Chords extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentChordName: 'D Major',
+      currentChord: [0, 4, 7],
+      blackKeys: [1, 3, 6, 8, 10, 13, 15, 18, 20, 22],
+      whiteKeys: [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23]
+    }
+    this.mapChordsTop = this.mapChordsTop.bind(this);
+    this.mapChordsBot = this.mapChordsBot.bind(this);
+    this.mapChordNames = this.mapChordNames.bind(this);
+    this.clickChord = this.clickChord.bind(this);
+  }
+  clickChord(currentChord) {
+    this.setState({
+      currentChordName: currentChord[0],
+      currentChord: currentChord[1]
+    });
+    this.mapChordsTop(currentChord);
+    this.mapChordsBot(currentChord);
+  }
+  mapChordNames(chord) {
+    return <div onClick={() => this.clickChord(chord)}>{chord[0]}</div>
+  }
+  mapChordsTop(chordNumber) {
+    // C# D#     F# G# A#
+    // 1  3      6  8  10
+    const blackKeys = {
+      1: 'C#',
+      3: 'D#',
+      6: 'F#',
+      8: 'G#',
+      10: 'A#',
+      13: 'C#',
+      15: 'D#',
+      18: 'F#',
+      20: 'G#',
+      22: 'A#'
+    }
+    if (this.state.currentChord.includes(chordNumber)) {
+      return <div className="note" style={{ color: 'red' }}>{blackKeys[chordNumber]}</div>
+    } else {
+      return <div></div>
+    }
+  }
+  mapChordsBot(chordNumber) {
+    // C D E F G A B
+    // 0 2 4 5 7 9 11
+    const whiteKeys = {
+      0: 'C',
+      2: 'D',
+      4: 'E',
+      5: 'F',
+      7: 'G',
+      9: 'A',
+      11: 'B',
+      12: 'C',
+      14: 'D',
+      16: 'E',
+      17: 'F',
+      19: 'G',
+      21: 'A',
+      23: 'B'
+    }
+    if (this.state.currentChord.includes(chordNumber)) {
+      return <div className="note">{whiteKeys[chordNumber]}</div>
+    } else {
+      return <div></div>
+    }
+  }
+
+  render() {
+    return (
+      <div className="piano-container">
+        <div className="chord-list">
+          <div>
+            <strong>Chords in Key - {this.props.currentChord}</strong>
+          </div>
+          {this.props.chords.map(this.mapChordNames)}
+        </div>
+        <div className="piano-grid">
+          <div className="piano-grid-top">
+            {this.state.blackKeys.map(this.mapChordsTop)}
+          </div>
+          <div>
+            <strong>Current Chord: {this.state.currentChordName}</strong>
+          </div>
+          <div className="piano-grid-bot">
+            {this.state.whiteKeys.map(this.mapChordsBot)}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Chords;
--- server/index.js
+++ server/index.js
@@ -3,15 +3,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const scales = require('../database-mongo');
 const db = require('../database-mongo/index.js');
-const port = 3001;
+const port = 3000;
 
 const app = express();
 
 
 app.use(express.static(path.join(__dirname + '/../react-client/dist')));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.get('/api/scales/:chord', function (req, res) {
   const chord = req.params.chord
@@ -25,7 +23,6 @@ app.get('/api/scales/:chord', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!');
+app.listen(port, function () {
+  console.log(`listening on port ${port}.`);
 });
-
